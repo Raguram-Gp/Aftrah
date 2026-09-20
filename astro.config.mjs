1130ca89774086f35357d20156fac4500d8309ab
raguram-gp <raguram10hb@gmail.com>
2026-09-20 14:37:28 +0530

    SEO changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://aftrah.vercel.app',
   server: {
     host: true,
   },
-  integrations: [react()]
+  integrations: [
+    react(),
+    sitemap({
+      filter: (page) =>
+        !page.includes('/afrah-app') &&
+        !page.includes('/aftrah-app') &&
+        !page.includes('/s') &&
+        !page.includes('/404'),
+    }),
+  ],
 });
 
